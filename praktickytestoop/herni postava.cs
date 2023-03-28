bda17def800b03511cd6675104eea19f6455c3d7
Radek Křikava <krikavar.03@spst.cz>
2023-03-28 12:00:21 +0200

    Přidejte soubory projektu.

---
+++ praktickytestoop/herni postava.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace praktickytestoop
+{
+
+
+    class herni_postava
+    {
+        private string jmeno;
+        private int lvl;
+        private int x;
+        private int y;
+
+        public herni_postava(string jmeno)
+        {
+            jmeno = jmeno;
+            lvl = 1;
+            x = 0;
+            y = 0;
+        }
+        public string Name
+        {
+            get => jmeno;
+           
+        }
+    }
+}
+
